f05ee0b3e12229e3b8ad20dacf6f50d75be16a54
Nima Poshtiban <poshtibannima@gmail.com>
2022-05-23 19:04:01 +0430

    Completed CRUD actions with views for Category

---
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -0,0 +1,113 @@
+﻿using BookStoreWeb.Data;
+using BookStoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreWeb.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public async Task<IActionResult> Index()
+    {
+        var data = await _db.Categories.ToListAsync();
+        return View(data);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Create()
+    {
+        return await Task.FromResult<IActionResult>(View());
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "The Display order cannot exactly match the Name");
+        }
+        if (!ModelState.IsValid)
+        {
+            return View(obj);
+        }
+        await _db.Categories.AddAsync(obj);
+        await _db.SaveChangesAsync();
+        TempData["Success"] = "Category Created Successfully";
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var category = await _db.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+        return View(category);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "The Display order cannot exactly match the Name");
+        }
+        if (!ModelState.IsValid)
+        {
+            return View(obj);
+        }
+
+        _db.Categories.Update(obj);
+        await _db.SaveChangesAsync();
+        TempData["Success"] = "Category Updated Successfully";
+        return RedirectToAction(nameof(Index));
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var category = await _db.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+        return View(category);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeletePOST(int? id)
+    {
+        var obj = await _db.Categories.FindAsync(id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+
+        _db.Categories.Remove(obj);
+        await _db.SaveChangesAsync();
+        TempData["Success"] = "Category Deleted Successfully";
+        return RedirectToAction(nameof(Index));
+    }
+}
