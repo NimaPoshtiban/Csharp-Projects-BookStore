ec833481b410ef1e8bdaf40aec267fc68ab3f496
Nima Poshtiban <poshtibannima@gmail.com>
2022-05-26 15:59:22 +0430

    change Home page and added details page

--- BookStoreWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookStoreWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,24 +1,37 @@
 ﻿using BookStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models.ViewModels;
 
 
 namespace BookStoreWeb.Areas.Customer.Controllers;
 [Area("Customer")]
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+
+    public HomeController(IUnitOfWork unitOfWork)
     {
-        _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var productList = await  _unitOfWork.Product.GetAllAsync(includeProperties:"Category,CoverType");
+        return View(productList);
     }
 
+    public async Task<IActionResult> Details(int id)
+    {
+        var cartObj = new ShoppingCart
+        {
+            Product = await _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, "Category,CoverType"),
+            Count = 1
+        };
+        return View(cartObj);
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
